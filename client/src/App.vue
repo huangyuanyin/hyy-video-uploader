72f72e9c5513802d842024d22952c5ef25786093
3443718488@qq.com <3443718488@qq.com>
2022-07-24 00:09:49 +0800

    部分功能代码

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,23 @@
-<script setup>
-</script>
-
 <template>
-  <div></div>
+  <p>
+    <progress id="uploaderProgress" value="2" max="100"></progress>
+  </p>
+  <p>
+    <input type="file" id="videoUploader" value="" />
+  </p>
+  <p>
+    <span id="uploadInfo"></span>
+  </p>
+  <p>
+    <button id="uploadBtn">上传视频</button>
+  </p>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const inputName = ref("选择视频")
+</script>
 
 <style scoped>
 </style>
---
+++ client/src/utils/videoUploader.js
@@ -0,0 +1,42 @@
+const UPLOAD_INFO = {
+  'NO_FILE': '请先选择文件',
+  'INVALID_TYPE': '不支持该类型文件上传'
+};
+
+const ALLOWED_TYPE = {
+  'video/mp4': 'mp4',
+  'video/ogg': 'ogg'
+};
+
+((doc) => {
+  const oProgress = doc.querySelector("#uploaderProgress");
+  const oUploader = doc.querySelector("#videoUploader");
+  const oInfo = doc.querySelector("#uploadInfo");
+  const oBtn = doc.querySelector("#uploadBtn");
+
+  const init = () => {
+    bindEvent()
+  }
+
+  function bindEvent() {
+    oBtn.addEventListener('click', uploadVideo, false)
+  }
+
+  function uploadVideo() {
+    // const file = oUploader.files[0]
+    const { files: [file] } = oUploader
+    console.log(file)
+    if (!file) {
+      oInfo.innerText = UPLOAD_INFO['NO_FILE']
+      return;
+    }
+    // 类型支持
+    if (!ALLOWED_TYPE[file.type]) {
+      oInfo.innerText = UPLOAD_INFO['INVALID_TYPE']
+      return;
+    }
+    const { name, size, type } = file
+  }
+
+  init();
+})(document);
