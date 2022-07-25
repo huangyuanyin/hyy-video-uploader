f7647b7d6232fd11fc2146100328e5fb7209086d
liuzhanghao <liuzhanghao@infosec.com.cn>
2022-07-25 09:53:00 +0800

    前端功能代码

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,11 +1,19 @@
+const BASE_URL = "http://localhost:8000";
+
 export const UPLOAD_INFO = {
-    'NO_FILE': '请先选择文件',
-    'INVALID_TYPE': '不支持该类型文件上传'
+  NO_FILE: "请先选择文件",
+  INVALID_TYPE: "不支持该类型文件上传",
+  UPLOAD_FAILED: "上传失败",
+  UPLOAD_SUCCESS: "上传成功",
 };
 
 export const ALLOWED_TYPE = {
-    'video/mp4': 'mp4',
-    'video/ogg': 'ogg'
+  "video/mp4": "mp4",
+  "video/ogg": "ogg",
 };
 
 export const CHUNK_SIZE = 64 * 1024;
+
+export const API = {
+  UPLOAD_VIDEO: BASE_URL + "upload_video",
+};
--- client/src/utils/videoUploader.js
+++ client/src/utils/videoUploader.js
@@ -1,4 +1,4 @@
-import { UPLOAD_INFO, ALLOWED_TYPE, CHUNK_SIZE } from './config'
+import { UPLOAD_INFO, ALLOWED_TYPE, CHUNK_SIZE, API } from "./config";
 
 ((doc) => {
   const oProgress = doc.querySelector("#uploaderProgress");
@@ -9,62 +9,67 @@ import { UPLOAD_INFO, ALLOWED_TYPE, CHUNK_SIZE } from './config'
   let uploadedSize = 0; // 当前上传了多少
 
   const init = () => {
-    bindEvent()
-  }
+    bindEvent();
+  };
 
   function bindEvent() {
-    oBtn.addEventListener('click', uploadVideo, false)
+    oBtn.addEventListener("click", uploadVideo, false);
   }
 
-  function uploadVideo() {
+  async function uploadVideo() {
     // const file = oUploader.files[0]
-    const { files: [file] } = oUploader
-    console.log(file)
+    const {
+      files: [file],
+    } = oUploader;
+    console.log(file);
     if (!file) {
-      oInfo.innerText = UPLOAD_INFO['NO_FILE']
+      oInfo.innerText = UPLOAD_INFO["NO_FILE"];
       return;
     }
     // 类型支持
     if (!ALLOWED_TYPE[file.type]) {
-      oInfo.innerText = UPLOAD_INFO['INVALID_TYPE']
+      oInfo.innerText = UPLOAD_INFO["INVALID_TYPE"];
       return;
     }
-    const { name, size, type } = file // 解构出文件名称，大小，类型
+    const { name, size, type } = file; // 解构出文件名称，大小，类型
 
-    const fileName = new Date().getTime() + '_' + name; // 切出来的文件名
+    const fileName = new Date().getTime() + "_" + name; // 切出来的文件名
+    const uploadedResult = {};
 
     oProgress.max = size;
-    oInfo.innerText = '';
+    oInfo.innerText = "";
 
     while (uploadedSize < size) {
-      const fileChunk = file.size(uploadedSize, uploadedSize + CHUNK_SIZE)
+      const fileChunk = file.size(uploadedSize, uploadedSize + CHUNK_SIZE);
       const formData = createFormDate({
         name,
         type,
-        size
+        size,
         fileName,
         uploadedSize,
-        file: fileChunk
-      })
+        file: fileChunk,
+      });
+      try {
+        uploadedResult = await axios.post(API.UPLOAD_VIDEO, formData);
+      } catch (error) {
+        oInfo.innerText = `${UPLOAD_INFO["UPLOAD_FAILED"]}(${error.message})`;
+        return;
+      }
+      uploadedSize += fileChunk.size;
+      oProgress.value = uploadedSize;
     }
-
+    oInfo.innerText = UPLOAD_INFO["UPLOAD_SUCCESS"];
+    oUploader.value = null;
   }
 
-  function createFormDate({
-    name,
-    type,
-    size
-    fileName,
-    uploadedSize,
-    file
-  }) {
-    const fd = new FormData()
-    fd.append('name', name)
-    fd.append('type', type)
-    fd.append('size', size)
-    fd.append('fileName', fileName)
-    fd.append('uploadedSize', uploadedSize)
-    fd.append('file', file)
+  function createFormDate({ name, type, size, fileName, uploadedSize, file }) {
+    const fd = new FormData();
+    fd.append("name", name);
+    fd.append("type", type);
+    fd.append("size", size);
+    fd.append("fileName", fileName);
+    fd.append("uploadedSize", uploadedSize);
+    fd.append("file", file);
 
     return fd;
   }
